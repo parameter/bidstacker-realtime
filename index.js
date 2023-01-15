dad18b0c41c417900a4271058a9a2b99fdd4bb72
parameter <paer.henriksson@yahoo.com>
2023-01-15 13:21:39 +0100

    ws round 99

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const path = require('path');
 const { createServer } = require('http');
-
-const WebSocket = require('ws');
+const corsMiddleware = require('./cors');
 
 const app = express();
+app.use(corsMiddleware);
+
+const WebSocket = require('ws');
 
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function (ws) {
-
-  console.log('MF connection just happened');
-
-
+  console.log('MF connection just happened', ws);
 });
 
 server.listen(8080, function () {
   console.log('Listening on http://0.0.0.0:8080');
 });
+
