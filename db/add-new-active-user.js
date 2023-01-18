290b87ad34c2289c5bf528153d94058d627f0d29
parameter <paer.henriksson@yahoo.com>
2023-01-18 19:45:26 +0100

    ytrewq668

--- db/add-new-active-user.js
+++ db/add-new-active-user.js
@@ -1,9 +1,10 @@
+const getMongoDb = require('./db.js');
 
 
+const addNewActiveUser = async () => {
+    const db = await getMongoDb();
+}
 
+// db.collection('test').insertOne({'ytrewq': 666})
 
-
-db.collection('test').insertOne({'ytrewq': 666})
-
-
-
+module.exports = addNewActiveUser;
