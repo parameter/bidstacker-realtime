4bea39d26c18431ef2de7d5895bb0e5c9fbba5f3
parameter <paer.henriksson@yahoo.com>
2023-01-19 15:01:20 +0100

    8734857348573847554343

--- index.js
+++ index.js
@@ -14,16 +14,19 @@ var app = express();
   app.options('*', corsMiddleware);
   app.use(corsMiddleware);
   app.use(bodyParser.json());
-
+ 
   // sockets 
   var server = require('http').Server(app)
   var io = require('socket.io')(server);
+
+  const socket = io('https://bidstacker.vercel.app', {transports: ['websocket']});
 
   io.on("connection", (socket) => {
     console.log('WE HAVE A CONNECTION', socket);
   });
 
 
+  
   // routes
   app.get('/', async (req, res) => {
       res.send({result: 'I am the result'});
